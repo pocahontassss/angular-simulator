398a685afdaca85931592a6b0eaf5348459f2bc8
Nakir <nakir3000@gmail.com>
2026-01-10 18:06:32 +0300

    14-homework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import './training.js';
 
 @Component({
   selector: 'app-root',
   imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  
 })
 export class AppComponent {
 
