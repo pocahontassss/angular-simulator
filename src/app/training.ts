cc79696f2ebfee9981806de036fce12a398d10e5
Galim Akhmedzhanov <akhmedzhanow@gmail.com>
2026-01-18 01:36:46 +0400

    fixed issues

--- src/app/training.ts
+++ src/app/training.ts
@@ -1,61 +1,59 @@
 interface IUser {
-    name: string;
-    age: number;
-    email?: string;
+  name: string;
+  age: number;
+  email?: string;
 }
 
 interface IAdmin extends IUser {
-    role: string;
-    experience: number;
+  role: string;
+  experience: number;
 }
 
-let admin: IAdmin = {
-    name: "Galim",
-    age: 27,
-    role: "Admin",
-    experience: 1
-}
+const admin: IAdmin = {
+  name: 'Galim',
+  age: 27,
+  role: 'Admin',
+  experience: 1,
+};
 
-let status1: "loading" | "success" | "error";
-status1 = "success";
+let status1: 'loading' | 'success' | 'error' = 'success';
 
-let textFormat: "uppercase" | "lowercase" | "capitalize";
-textFormat = "uppercase"
+let textFormat: 'uppercase' | 'lowercase' | 'capitalize' = 'uppercase';
 
-function sum(a: number, b: number):number {
-    return a + b
+function sum(a: number, b: number): number {
+  return a + b;
 }
 
 console.log(sum(5, 7));
 
-function formatString (text: string, mode: "uppercase" | "lowercase" | "capitalize"): string {
-    if(mode === "uppercase") {
-        return text.toUpperCase();
-    }
+function formatText(text: string, format: 'uppercase' | 'lowercase' | 'capitalize'): string {
+  if (format === 'uppercase') {
+    return text.toUpperCase();
+  }
 
-    if(mode === "lowercase") {
-        return text.toLowerCase();
-    }
+  if (format === 'lowercase') {
+    return text.toLowerCase();
+  }
 
-    if(mode === "capitalize") {
-        return text[0].toUpperCase() + text.slice(1);
-    }
+  if (format === 'capitalize') {
+    return text[0].toUpperCase() + text.slice(1);
+  }
 
-    return text
+  return text;
 }
 
-console.log(formatString("Angular", "uppercase"));
+console.log(formatText('Angular', 'uppercase'));
 
 function removeSymbol(text: string, symbol: string): string {
-  return text.replaceAll(symbol, "");
+  return text.replaceAll(symbol, '');
 }
 
-console.log(removeSymbol("Angular!", "!"));
+console.log(removeSymbol('Angular!', '!'));
 
 const users: IUser[] = [
-  { name: "Галим", age: 27, email: "Galim@mail.com" },
-  { name: "Амиры", age: 35 },
-  { name: "Али", age: 21, email: "Ali@mail.com" }
+  { name: 'Галим', age: 27, email: 'Galim@mail.com' },
+  { name: 'Амиры', age: 35 },
+  { name: 'Али', age: 21, email: 'Ali@mail.com' },
 ];
 
 const adults = users.filter((user) => user.age > 21);
