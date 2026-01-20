211daaaf8a64099c1a75422655067a24251b2d77
MusaSagov <sagov82@mail.ru>
2026-01-21 00:32:11 +0300

    fix error

--- src/app/training.ts
+++ src/app/training.ts
@@ -7,8 +7,6 @@ interface IUser {
 }
 
 interface IDeveloper extends IUser {
-  id: number;
-  age?: number;
   github: string;
   skills: string[];
 }
@@ -16,11 +14,12 @@ interface IDeveloper extends IUser {
 //Переменные.
 type Status = 'loading' | 'success' | 'error';
 type textFormat = 'uppercase' | 'lowercase' | 'capitalize';
+type IAdult = IUser & { age: number };
 
 //Функции
 function sum(a: number, b: number): number {
-  const result = a + b;
-  console.log(`Sum: ${result}`);
+  const result: number = a + b;
+  console.log(`Sum: ${ result }`);
   return result;
 }
 
@@ -29,11 +28,11 @@ sum(3, 6);
 function formatText(text: string, format: textFormat): string {
   if (format === 'uppercase') {
     return text.toUpperCase();
-  }
-  if (format === 'lowercase') {
+  }  else if (format === 'lowercase') {
     return text.toLowerCase();
+  } else {
+    return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
   }
-  return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
 }
 
 console.log(formatText("hello world", "capitalize"));
@@ -41,7 +40,9 @@ console.log(formatText("hello world", "uppercase"));
 console.log(formatText("hello world", "lowercase"));
 
 function removeChar(text: string, char: string): string {
-  if (!char) return text
+  if (!char) {
+     return text
+  }
   return text.replaceAll(char, '');
 }
 
@@ -52,10 +53,13 @@ console.log(removeChar("test@@test", "@"));
 //Массив.
 const users: IUser[] = [
   { id: 1, name: 'Алексей Смирнов', email: 'alex@dev.ru', age: 28 },
-  { id: 2, name: 'Мария Петрова', email: 'maria@test.ru' },
+  { id: 2, name: 'Мария Петрова', email: 'maria@test.ru', age: 30 },
   { id: 3, name: 'Иван Иванов', email: 'ivan@mail.ru', age: 22 },
   { id: 4, name: 'Елена Козлова', email: 'elena@work.ru', age: 35 }
 ]
 
-const adults: IUser[] = users.filter(user => user.age && user.age > 25);
+const adults: IAdult[] = users.filter((user): user is IAdult =>
+  user.age !== undefined && user.age > 25
+);
+
 console.log(adults);
