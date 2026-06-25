18ef5322066454d96406f629ba37814902d82045
Adel Bikkenin <autopluskazan@gmail.com>
2026-06-25 15:32:05 +0300

    added a collection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,13 @@ export class AppComponent {
     console.log(users.getDeterminate(2));
     users.replace(1, 'Жан-Поль');
     users.remove(0);
-    console.log(users.getAll());
+
+    const phoneNumbers = new Collection<string> (['+7-904-777-41-74', '+7-917-857-54-11', '+7-908-528-41-80', '+7-903-342-85-75', '+7-901-854-44-00']);
+    console.log(phoneNumbers.getAll());
+    console.log(phoneNumbers.getDeterminate(4));
+    phoneNumbers.replace(2, '2-525-854');
+    phoneNumbers.clear();
+    console.log(phoneNumbers.getAll());
   }
 
   companyName: string = 'румтибет';
