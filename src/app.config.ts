ebb0883ca5f3c001c337ff105bcc9ed6546e9069
Gulnara <gulnarakhasanova82@gmail.com>
2026-07-08 09:57:33 +0300

    Исправила файлы и подготовила к сдачи

---
+++ src/app.config.ts
@@ -0,0 +1,12 @@
+import '../../training';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes)
+  ]
+};
