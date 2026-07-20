fbe17b7ef18aeec93e1324f26604ccadccc0ccfb
msalah-hub <csagokum@mail.ru>
2026-07-20 03:35:38 +0300

    fix mistakes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,76 +21,16 @@ interface User {
 })
 export class AppComponent {
 
-  companyName: string = "РУМТИБЕТ"
-
-  isPrimaryColors(color: Colors): boolean {
-    if (color === Colors.BLUE || color === Colors.GREEN || color === Colors.RED) {
-      return true;
-    } else {
-      return false;
-    };
-  };
-
-  testColors() {
-    console.log(this.isPrimaryColors(Colors.BLUE))
-    console.log(this.isPrimaryColors(Colors.GREEN))
-    console.log(this.isPrimaryColors(Colors.RED))
-  };
-
-  constructor() {
-    this.saveSunsetTime();
-    this.quantityVisitToSite();
-    this.initCollections();
-
-    // таймер в шапе 16 дз, 4 задание
-    this.updateClock();
-
-    setInterval(() => {
-      this.updateClock();
-    }, 1000);
-  
-  }
-
-  saveSunsetTime(): void {
-    const time = new Date();
-    const timeString = time.toISOString();
-    localStorage.setItem('last_visit_time', timeString)
-  }
-
-  quantityVisitToSite(): void {
-    const storageKey = 'visit_to_site';
-    const currentVisitToSite = localStorage.getItem(storageKey);
-
-    let newCount: number;
-
-    if ( currentVisitToSite === null) {
-      newCount = 1;
-    } else {
-      newCount = parseInt(currentVisitToSite, 10) + 1;
-    }
-
-    localStorage.setItem(storageKey, newCount.toString());
-    
-    console.log(`Кол-во заходов ${newCount}`);
-  }
-
-  
-  initCollections() {
-    const numbersCollection = new Collection<number>([10,20,30])
-
-    console.log(numbersCollection.getAll());
-
-    const userCollection: User[] = [
-    {name: 'Vlad', id: 1},
-    {name: "Oleg", id: 2}
-  ]
-
-  const usersCollection = new Collection<User>(userCollection);
-
-  console.log(usersCollection.getAll())
-  }
-
-  featuresList = [
+  public companyName: string = "РУМТИБЕТ"
+  public location: string = '';
+  public date: string = '';
+  public participants: string = '';
+  public currentTime: string = '';
+  public counter: number = 0;
+  public showTimer: boolean = true;
+  public liveText: string = "";
+  public isLoading: boolean = true;
+  public featuresList = [
     {
       icon: "/images/guide-icon.svg ",
       title: "Опытный гид ",
@@ -108,24 +48,87 @@ export class AppComponent {
     }
   ];
 
-  location: string = '';
-  date: string = '';
-  participants: string = '';
+  constructor() {
+    this.saveSunsetTime();
+    this.quantityVisitToSite();
+    this.initCollections();
 
+    // таймер в шапке 16 дз, 4 задание
+    this.updateClock();
 
-  currentTime: string = '';
+    setInterval(() => {
+      this.updateClock();
+    }, 1000);
+  }
+
+  // 8 задание загрузка страницы 
+
+  private ngOnInit() {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 2000);
+  }
+
+  public isPrimaryColors(color: Colors): boolean {
+    if (color === Colors.BLUE || color === Colors.GREEN || color === Colors.RED) {
+      return true;
+    } else {
+      return false;
+    };
+  };
+
+  public testColors() {
+    console.log(this.isPrimaryColors(Colors.BLUE))
+    console.log(this.isPrimaryColors(Colors.GREEN))
+    console.log(this.isPrimaryColors(Colors.RED))
+  };
+
+  public saveSunsetTime(): void {
+    const time = new Date();
+    const timeString = time.toISOString();
+    localStorage.setItem('last_visit_time', timeString)
+  }
+
+  private quantityVisitToSite(): void {
+    const storageKey = 'visit_to_site';
+    const currentVisitToSite = localStorage.getItem(storageKey);
+
+    let newCount: number;
+
+    if ( currentVisitToSite === null) {
+      newCount = 1;
+    } else {
+      newCount = parseInt(currentVisitToSite, 10) + 1;
+    }
+
+    localStorage.setItem(storageKey, newCount.toString());
+    
+    console.log(`Кол-во заходов ${newCount}`);
+  }
 
   
+  private initCollections() {
+    const numbersCollection = new Collection<number>([10,20,30])
+
+    console.log(numbersCollection.getAll());
+
+    const userCollection: User[] = [
+    {name: 'Vlad', id: 1},
+    {name: "Oleg", id: 2}
+  ]
+
+  const usersCollection = new Collection<User>(userCollection);
+
+  console.log(usersCollection.getAll())
+  };
+
   // таймер в шапке 
 
-  updateClock() {
+  private updateClock() {
     const now = new Date();
 
     this.currentTime = now.toLocaleString();
   }
-
-
-  counter: number = 0;
 
   increment() {
     this.counter++;
@@ -139,35 +142,17 @@ export class AppComponent {
   
   // 6 задание 16 дз
 
-  showTimer: boolean = true;
-
-  toggleWidget() {
+  public toggleWidget() {
     this.showTimer = !this.showTimer;
   }
 
   // 7 задание 16 дз 
 
-  liveText: string = "";
-
-  updateText(event: Event) {
+  public updateText(event: Event) {
     const inputElement = event.target as HTMLInputElement;
     this.liveText = inputElement.value;
   }
-
-  // 8 задание загрузка страницы 
-
-  isLoading: boolean = true;
-
-  ngOnInit() {
-    setTimeout(() => {
-      this.isLoading = false;
-    }, 2000);
-  }
-
 };
-
-
-
 
 
 console.log(user);
