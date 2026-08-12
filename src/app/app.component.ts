5492bfe6ab5a1beadada7bd3399d2c1739e8e0bf
Mavlid <votkny93@gmail.com>
2026-08-12 11:30:09 +0300

    feature/16-homework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import './training';
+
 
 @Component({
   selector: 'app-root',
---
+++ src/app/training.ts
@@ -0,0 +1,44 @@
+function calcTotal(a: number, b: number): number {
+  return (a + b)
+};
+console.log(calcTotal(2, 5));
+
+let uploadStatus: 'loading' | 'success' | 'error';
+
+let textFormat: 'uppercase' | 'lowercase' | 'capitalize';
+
+interface User {
+  name: string;
+  age: number;
+  country?: string;
+};
+
+interface Vlad extends User {
+  gender: string;
+};
+
+function formatText(str: string, format: 'uppercase' | 'lowercase' | 'capitalize'): string {
+  if (format === 'uppercase') {
+    return str.toUpperCase();
+  }
+  if (format === 'lowercase') {
+    return str.toLowerCase();
+  }
+  if (format === 'capitalize') {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+  return str;
+};
+
+function removeChar(str: string, char: string): string {
+  return str.replaceAll(char, '')
+};
+
+const users: User[] = [
+  {name: 'ivan', age: 25, country: 'Russia'},
+  {name: 'Anna', age: 17, country: 'Russia'},
+  {name: 'Petr', age: 32, },
+  {name: 'Dmitry', age: 18, country: 'Russia'},
+];
+
+const adults = users.filter(user => user.age >=18);
