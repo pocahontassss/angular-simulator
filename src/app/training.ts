31bf01b560cc9e758fbe31ef97c72daef753d473
Ramzat <ramzat1982@gmail.com>
2026-09-01 08:11:03 +0600

    Исправил замечания

--- src/app/training.ts
+++ src/app/training.ts
@@ -1,4 +1,3 @@
-
 interface IUser {
   name: string;
   age: number;
@@ -9,46 +8,46 @@ interface IDeveloper extends IUser {
   programmingLanguage: string;
 }
 
-let uploadStatus:"loading" | "success" | "error";
+let uploadStatus: 'loading' | 'success' | 'error';
 
-let textFormat:"uppercase" | "lowercase" | "capitalize";
+let textFormat: 'uppercase' | 'lowercase' | 'capitalize';
 
-function getSumm (a: number, b: number): number {
-  return (a + b)
+function getSumm(a: number, b: number): number {
+  return a + b;
 }
 
-function getString (a: string, b:"uppercase" | "lowercase" | "capitalize"): string {
-  if (b === "uppercase"){
-    return a.toUpperCase()
+function getString(a: string, b: 'uppercase' | 'lowercase' | 'capitalize'): string {
+  switch (b) {
+    case 'uppercase':
+      return a.toUpperCase();
+
+    case 'lowercase':
+      return a.toLowerCase();
+
+    case 'capitalize':
+      return a[0].toUpperCase() + a.slice(1);
   }
-    if (b === "lowercase"){
-    return a.toLowerCase()
-  }
-    if (b === "capitalize"){
-    return a[0].toUpperCase() + a.slice(1)
-  }
-  return a
 }
 
-function delCharacter (a: string, b: string): string {
-  return a.replaceAll(b, "")
+function delCharacter(a: string, b: string): string {
+  return a.replaceAll(b, '');
 }
 
 let users: IUser[] = [
   {
-    name: "Ramzat",
+    name: 'Ramzat',
     age: 20,
-    password: "12345678"
+    password: '12345678',
   },
-    {
-    name: "Amir",
+  {
+    name: 'Amir',
     age: 25,
-    password: "12345678910"
+    password: '12345678910',
   },
-    {
-    name: "Egor",
+  {
+    name: 'Egor',
     age: 18,
-    password: "55555555"
-  }
-]
-const filteredUsers = users.filter(user => user.age > 20);
+    password: '55555555',
+  },
+];
+const filteredUsers: IUser[] = users.filter((user) => user.age > 20);
